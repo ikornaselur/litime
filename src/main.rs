b6c971b22c5f4fbdc1089e48f2a7b67c6ccf062e
Axel Örn Sigurðsson <axel@absalon.is>
2018-08-29 15:38:18 +0900

    Add support for custom timestamp in cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 extern crate chrono;
+extern crate clap;
 extern crate colored;
 extern crate rand;
+extern crate regex;
 extern crate textwrap;
 
 use chrono::prelude::*;
+use clap::{App, Arg};
 use colored::*;
+use regex::Regex;
 use textwrap::fill;
 
 use minute::get_minute;
@@ -12,6 +16,19 @@ use minute::get_minute;
 mod minute;
 
 fn main() {
+    let matches = App::new("Litime")
+        .version("0.2.0")
+        .about("Display a timestamp with a literature quote. By default, the current time stamp is used.")
+        .arg(
+            Arg::with_name("time")
+                .short("t")
+                .value_name("time")
+                .help("A timestamp to get a quote for, for example 07:16")
+                .validator(is_timestamp)
+                .takes_value(true),
+        )
+        .get_matches();
+
     let local: DateTime<Local> = Local::now();
     let now = format!(
         "{:0width$}:{:0width$}",
@@ -19,7 +36,10 @@ fn main() {
         local.minute(),
         width = 2
     );
-    let minute = get_minute(now);
+
+    let timestamp = matches.value_of("time").unwrap_or(&now);
+
+    let minute = get_minute(timestamp.to_string());
     let result = format!(
         "{}{}{}",
         minute.start.bright_black(),
@@ -42,4 +62,15 @@ fn main() {
     }
     println!("");
     println!("        {} - {}", minute.author, minute.title);
+}
+
+fn is_timestamp(val: String) -> Result<(), String> {
+    let re = Regex::new(r"^([01][0-9]|2[0-3]):[0-5][0-9]$").unwrap();
+    if re.is_match(&val) {
+        Ok(())
+    } else {
+        Err(String::from(
+            "the value must be a valid 24-hour timestamp in the format HH:MM",
+        ))
+    }
 }
