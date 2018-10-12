9440d5fbd60b2b55180aea9a693eaf8e44601531
Axel Örn Sigurðsson <axel@absalon.is>
2018-10-12 19:43:35 +0200

    Remove rustfmt skip from get_minute

--- src/minute.rs
+++ src/minute.rs
@@ -13,11 +13,15 @@ pub struct Minute {
 
 const RAW_TIMES: &str = include_str!("times.json");
 
-#[cfg_attr(rustfmt, rustfmt_skip)]
 pub fn get_minute(time: &str) -> Minute {
     let minutes: HashMap<&str, Vec<Minute>> = serde_json::from_str(&RAW_TIMES).unwrap();
 
-    let minute = minutes.get(time).unwrap_or_else(|| panic!("Couldn't find {:?}", time));
+    let minute = minutes
+        .get(time)
+        .unwrap_or_else(|| panic!("Couldn't find {:?}", time));
 
-    thread_rng().choose(&minute).expect("Unable to choose a random quote").clone()
+    thread_rng()
+        .choose(&minute)
+        .expect("Unable to choose a random quote")
+        .clone()
 }
