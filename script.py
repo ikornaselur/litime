64d4cef13843f9d4d07b2dc5a4851e8ba0e6783c
Axel Örn Sigurðsson <axel@absalon.is>
2018-10-12 19:30:29 +0200

    Read times from a json file instead of embed in source
    It slows run time down from 11ms to 15ms, but is much nicer to maintain

--- script.py
+++ script.py
@@ -10,21 +10,16 @@ path_to_files = "/tmp/literature-clock/docs/times"
 for json_file in os.listdir(path_to_files):
     with open(os.path.join(path_to_files, json_file), "r") as f:
         time_stamp = "_".join(json_file.split(".")[0].split("_"))
-        times[time_stamp] = json.load(f)
+        times[time_stamp.replace("_", ":")] = [
+            {
+                "start": d["quote_first"],
+                "time": d["quote_time_case"],
+                "end": d["quote_last"],
+                "title": d["title"],
+                "author": d["author"],
+            }
+            for d in json.load(f)
+        ]
 
-times2 = OrderedDict(sorted(times.items()))
-
-with open("/tmp/rusted", "w") as f:
-    for k, v in times2.items():
-        key = k.replace("_", ":")
-        f.write(f'    minutes.insert("{key}", &[\n')
-        for item in v:
-            first = item["quote_first"].replace('"', '\\"')
-            time = item["quote_time_case"].replace('"', '\\"')
-            rest = item["quote_last"].replace('"', '\\"')
-            author = item["author"].replace('"', '\\"')
-            title = item["title"].replace('"', '\\"')
-            f.write(
-                f'        &["{first}", "{time}", "{rest}", "{author}", "{title}"],\n'
-            )
-        f.write("    ]);\n")
+with open("./src/times.json", "w") as f:
+    json.dump(times, f)
