9a5a76c2f0806e5abbd419fefed276c4665efbcc
Axel Örn Sigurðsson <dev@absalon.is>
2022-09-15 23:51:51 +0100

    Add integration test to try all timestamps

---
+++ tests/integration_test.rs
@@ -0,0 +1,16 @@
+use litime::minute::get_minute;
+
+#[test]
+/// Get a quote 100 times for every possible timestamps.
+/// Each quote is retrieved 100 times to ensure (ish) that multiple random quotes for some
+/// timestamps don't cause any panics.
+fn test_get_minute() {
+    for hour in 1..24 {
+        for minute in 1..60 {
+            let timestamp = format!("{:0width$}:{:0width$}", hour, minute, width = 2);
+            for _ in 1..100 {
+                get_minute(&timestamp).expect(&timestamp);
+            }
+        }
+    }
+}
